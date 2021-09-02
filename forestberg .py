593fdf4563eb1a35308847c00331ce8ff7aef48a
Artstyle4 <Artstyle4@users.noreply.github.com>
2021-09-02 14:35:18 +0200

    webscraper for skogbergsantik.com

---
+++ forestberg .py
@@ -0,0 +1,80 @@
+import requests, os
+from bs4 import BeautifulSoup
+import tkinter as tk
+from PIL import Image, ImageTk
+import urllib
+from io import BytesIO
+
+
+#url som används för scrapingen
+
+#hitta platsen som scriptet körs ifrån
+dir_path = os.path.dirname(os.path.realpath(__file__))
+img_list = []
+
+
+#hämtar ett Beautiful-Soupobjekt från urlen
+def parse_site(url):
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, "html.parser")
+    return soup
+
+
+
+#skriver om htmlkoden till text och skriver till .csvfilen
+def get_imageUrl():
+    url = "http://www.skogbergsantik.com"
+    soup = parse_site(url)
+    galleryOne = soup.find('a',{'title' : 'Bildgalleri 1'})['href']
+    galleryTwo = soup.find('a',{'title' : 'Bildgalleri 2'})['href']
+    
+    return [galleryOne,galleryTwo]
+
+def get_images():
+    url = "http://www.skogbergsantik.com" + get_imageUrl()[1]
+    soup = parse_site(url)
+    images = soup.find_all('img')
+    for image in images:
+        if "files/120x120" in image['src']:
+            trimmed_image = image['src'].replace('/120x120','')
+            img_list.append("http://skogbergsantik.com/" + trimmed_image)
+    
+def select_photos():
+    row_number = 1
+    column_number = 0
+    
+    for image in img_list:
+       
+       print("working on item number: " + str(img_list.index(image)+1)+ "/" +str(len(img_list)))
+
+       URL = image
+       u = urllib.request.urlopen(URL)
+       raw_data = u.read()
+       u.close()
+       im = Image.open(BytesIO(raw_data))
+       resized_image = im.resize((250,250), Image.ANTIALIAS)
+
+       photo = ImageTk.PhotoImage(resized_image)
+       label = tk.Label(image=photo)
+       label.image = photo
+       label.grid(row=row_number, column=column_number)
+       column_number+=1
+       if((img_list.index(image)+1) % 5 == 0):
+           all_labels.append(label)
+           row_number += 1
+           column_number = 0
+           print(row_number)
+           
+
+
+
+#saves all images into img_list
+get_images()
+img_list = img_list[:50]
+all_labels = []
+root = tk.Tk()
+select_photos()
+
+
+
+root.mainloop()
